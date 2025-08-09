39bdd70b272ffecb6840b89cefc4af0abd4592b0
FAYRESU <664259005@webmail.npru.ac.th>
2025-08-09 23:02:17 +0700

    ทำไมกลับมาบ้านทำไมรันไม่ได้

--- client/src/service/restaurant.sevice.js
+++ client/src/service/restaurant.sevice.js
@@ -2,20 +2,22 @@ import api from "./api";
 const RESTO_API = import.meta.env.VITE_RESTO_API;
 
 //get all restaurant
-const getAllRestaurants = async () => {
-  return await api.get(RESTO_API);
-};
+// const getAllRestaurants = async () => {
+//   return await api.get(RESTO_API);
+// };
+
+const getAllRestaurants = () => api.get(RESTO_API);
 //get restaurant by id
 const getRestaurantById = async (id) => {
   return await api.get(`${RESTO_API}/${id}`);
 };
 //update restaurant by id
-const editRestaurantById = async (id, restaurant) => {
-  return await api.put(`${RESTO_API}/${id}`, restaurant);
+const editRestaurantById = async (id, restaurants) => {
+  return await api.put(`${RESTO_API}/${id}`, restaurants);
 };
 //add restaurant
-const insertRestaurant = async (restaurant) => {
-  return await api.post(RESTO_API, restaurant);
+const insertRestaurant = async (restaurants) => {
+  return await api.post(RESTO_API, restaurants);
 };
 //delete restaurant
 const deleteRestaurant = async (id) => {
